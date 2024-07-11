fe9069d85a3c4a80fe9b393bfeb6b59eeaa2da71
Adriasu <adriana.suarez@students.gammatech.school>
2024-07-11 16:53:22 +0200

    tarjetas creadas, dando estlo

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,7 @@ import React from "react";
 import CreditCard from "./components/CreditCard";
 
 const App = () => {
-  return (
-    <div>
-      <CreditCard
-        type={"visa"}
-        number={"12364"}
-        expirationMonth={12}
-        expirationYear={26}
-        bank={"BBVA"}
-        owner={"adriana"}
-        cssClass={"border-gray-900 bg-orange-400"}
-      />
-    </div>
-  );
+  return <CreditCard />;
 };
 
 export default App;
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,23 +1,63 @@
-import React from 'react'
+import React from "react";
 
-const CreditCard = ({type, number, expirationMonth, expirationYear, bank, owner, cssClass }) => {
-
-    const creditCards = [
-        {type}, {number}, {expirationMonth}, {expirationYear}, {bank}, {owner}, {cssClass}
-    ]
-
+const CreditCard = () => {
+  const creditCards = [
+    {
+      type: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Visa_Logo.png/640px-Visa_Logo.png",
+      number: "1236478512548845",
+      expirationMonth: 11,
+      expirationYear: 26,
+      bank: "BBVA",
+      owner: "Maxence Bouret",
+      cssClass: "h-40 w-72 bg-green-400 rounded-2xl text-white flex flex-col",
+    },
+    {
+      type: "https://upload.wikimedia.org/wikipedia/commons/b/b7/MasterCard_Logo.svg",
+      number: "1236478512540995",
+      expirationMonth: 10,
+      expirationYear: 28,
+      bank: "N26",
+      owner: "Adriana Suárez",
+      cssClass: "h-40 w-72 bg-gray-400 rounded-2xl text-black flex flex-col",
+    },
+    {
+      type: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Visa_Logo.png/640px-Visa_Logo.png",
+      number: "1236478512546984",
+      expirationMonth: 12,
+      expirationYear: 25,
+      bank: "Revolut",
+      owner: "Max Beltran",
+      cssClass: "h-40 w-72 bg-yellow-500 rounded-2xl text-white flex flex-col",
+    },
+  ];
 
   return (
-    <div>
+    <div className="flex gap-8 justify-center m-16">
+      {creditCards.map((cart, i) => {
+        return (
+          <div key={i}>
+            <div className="h-40 w-72 bg-yellow-500 rounded-2xl text-white flex flex-col items-start  ">
+              <p>hola</p>
+            </div>
 
-        {creditCards.map((dato, i) => {
-            return <div key={i}>{dato}</div>
-        })}
-
-
-      
+            <div className={cart.cssClass}>
+              <div className="flex justify-end">
+                <img src={cart.type} alt="logo" className="h-5 w-16" />
+              </div>
+              <p>{cart.number}</p>
+              <div className="flex">
+                <p>
+                  Expires {cart.expirationMonth}/{cart.expirationYear}
+                </p>
+                <p>{cart.bank}</p>
+              </div>
+              <p>{cart.owner}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default CreditCard
+export default CreditCard;
